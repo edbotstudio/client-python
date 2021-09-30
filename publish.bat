496b7dd3976f0067db25a773ea4ed377b11bcdc6
edbotstudio <studio@ed.bot>
2021-09-30 19:40:02 +0100

    Added publish script for convenience.

---
+++ publish.bat
@@ -0,0 +1,7 @@
+::
+:: Publish the package to PyPi and clean up afterwards.
+::
+
+python setup.py bdist_wheel 
+twine upload dist/* 
+rm -rf ./build ./dist ./*egg-info
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
 	name = "edbotstudio",
-	version = "1.0.0",
+	version = "1.0.1",
 	description = "A Python client API for Edbot Studio",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
